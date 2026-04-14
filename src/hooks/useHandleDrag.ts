60e5db4b9ebf0e7ac8e6392ed97b85e01444e54d
Artem-Litkovskyi <litkovskyi.artem@gmail.com>
2026-04-14 16:35:51 +0300

    Change useState to useRef for useHandleDrag internal values

--- src/hooks/useHandleDrag.ts
+++ src/hooks/useHandleDrag.ts
@@ -1,49 +1,49 @@
-import React, { useState } from 'react';
+import React, { useRef } from 'react';
 import type { Vec2 } from '../geometry/vec2.ts';
 import type { DragContext, Handle } from '../handles/Handle.ts';
 import { screenToWorld, type Transform } from '../utils/svg.ts';
 
 export function useHandleDrag(svg: SVGSVGElement | null, transform: Transform) {
-    const [activeHandle, setActiveHandle] = useState<Handle | null>(null);
-    const [startMouse, setStartMouse] = useState<Vec2 | null>(null);
-    const [prevMouse, setPrevMouse] = useState<Vec2 | null>(null);
+    const activeHandle = useRef<Handle | null>(null);
+    const startMouse = useRef<Vec2 | null>(null);
+    const prevMouse = useRef<Vec2 | null>(null);
 
     const onHandleDragStart = (handle: Handle, e: React.MouseEvent<SVGElement>) => {
         if (!svg) return;
 
         const p = screenToWorld(e.clientX, e.clientY, svg, transform);
 
-        setStartMouse(p);
-        setPrevMouse(p);
-        setActiveHandle(handle);
+        startMouse.current = p;
+        prevMouse.current = p;
+        activeHandle.current = handle;
 
         handle.onMouseDown?.();
     };
 
     const onHandleDrag = (e: React.MouseEvent<SVGSVGElement>) => {
-        if (!svg || !activeHandle || !startMouse || !prevMouse) return;
+        if (!svg || !activeHandle.current || !startMouse.current || !prevMouse.current) return;
 
         const p = screenToWorld(e.clientX, e.clientY, svg, transform);
 
         const ctx: DragContext = {
-            startMouse,
+            startMouse: startMouse.current,
             currentMouse: p,
             delta: {
-                x: p.x - prevMouse.x,
-                y: p.y - prevMouse.y,
+                x: p.x - prevMouse.current.x,
+                y: p.y - prevMouse.current.y,
             },
         };
 
-        activeHandle.onDrag(ctx);
+        activeHandle.current.onDrag(ctx);
 
-        setPrevMouse(p);
+        prevMouse.current = p;
     };
 
     const onHandleDragEnd = () => {
-        activeHandle?.onDragEnd?.();
-        setActiveHandle(null);
-        setStartMouse(null);
-        setPrevMouse(null);
+        activeHandle.current?.onDragEnd?.();
+        activeHandle.current = null;
+        startMouse.current = null;
+        prevMouse.current = null;
     };
 
     return { onHandleDragStart, onHandleDrag, onHandleDragEnd };
